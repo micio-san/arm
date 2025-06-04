885d3ae0b212367606c0f3a4412fbef68841bbdc
Mist Uber <106929734+micio-san@users.noreply.github.com>
2025-06-04 11:12:14 +0200

    Update ldr_str.s

--- ldr_str.s
+++ ldr_str.s
@@ -6,19 +6,19 @@ _start:
 //------------LDR--------------
 	//The '=' is used to get the address of the label, which is what i
 	//is stored 
-    ldr r0,=var1
+   LDR r0,=var1
 	//extracting the value that is currently store at r0 location,
 	//
-	ldr r1,[r0]
+   LDR r1,[r0]
  //segment name, any complex data that is not stored directly into a register
  //like arrays or strings
  //________STR_______________
  //used to store manipulare,store 
-    mov r2,#3
+    MOV r2,#3
 	//load the addy of the var2 to use it as destination
-	ldr r3, =var2
+	LDR r3, =var2
 	//source register, value i want to get, destination register
-	str r2, [r3]
+	STR r2, [r3]
 .data
 
 //2 bytes Data definition directives
